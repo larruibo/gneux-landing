c50cd138495a3311316d0c83f480854dfee1aa36
Luis Ruiz <la.ruiz967@uniandes.edu.co>
2017-08-25 19:40:56 -0500

    Casi terminado2

--- js/index.js
+++ js/index.js
@@ -2,6 +2,24 @@ jQuery(document).ready(function(){
 
   alert("Funciona JQuery");
 
+  var myIndex = 0;
+  carousel();
+
+  function carousel() {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      var y = document.getElementsByClassName("img-centered");
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";
+         y[i].style.display = "none";
+      }
+      myIndex++;
+      if (myIndex > x.length) {myIndex = 1}
+      x[myIndex-1].style.display = "block";
+      y[myIndex-1].style.display = "block";
+      setTimeout(carousel, 9000);
+    }
+
   $('#boton-primera').click( function() {
       $(window).scrollTo('#segunda', 500);
     }
